8cb9609ce5d0f14c757fcbfabfa418c392fb5b1d
Muhammad Hasnain <40852016+hasniansaleem@users.noreply.github.com>
2020-06-05 01:04:41 +0500

    Muhammad hasnain Bootcamp

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Muhammad Hasnain</h1>
+        <p>Welcome to React community</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
