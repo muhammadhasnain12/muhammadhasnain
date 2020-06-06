e783123da3a0e5ba40aed08c2cfdd57258ca2eee
Muhammad Hasnain <40852016+hasniansaleem@users.noreply.github.com>
2020-06-06 13:58:56 +0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Hello Muhammad Hasnain</h1>
         <p>Welcome to React community & Github Community :)</p>
+        <p>Bootcamp is going on with love</p>
       </header>
     </div>
   );
